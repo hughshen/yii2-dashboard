676fd78a659929793c0e7bf9491d97b2647b6d17
hughshen <hughssss@outlook.com>
2020-08-26 11:43:57 +0800

    Fix bug

--- backend/modules/cms/views/category/view.php
+++ backend/modules/cms/views/category/view.php
@@ -13,7 +13,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <p>
         <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id, 'parent' => $this->context->parent], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a(Yii::('app', 'Delete'), ['delete', 'id' => $model->id, 'parent' => $this->context->parent], [
+        <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id, 'parent' => $this->context->parent], [
             'class' => 'btn btn-danger',
             'data' => [
                 'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
